05a3371c714f89b10216fc7f1127d35b92c44685
Jason <jason@vinova.sg>
2018-03-07 22:36:50 +0700

    UI

---
+++ app/src/main/java/com/example/anhtuan/quanlyphongtro/TimTro.java
@@ -0,0 +1,13 @@
+package com.example.anhtuan.quanlyphongtro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimTro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tim_tro);
+    }
+}
