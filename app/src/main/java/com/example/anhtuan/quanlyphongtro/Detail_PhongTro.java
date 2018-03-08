5d31068228f101d5c60ac16c246b8e22ba06ea14
Jason <jason@vinova.sg>
2018-03-08 21:08:43 +0700

    UI

---
+++ app/src/main/java/com/example/anhtuan/quanlyphongtro/Detail_PhongTro.java
@@ -0,0 +1,13 @@
+package com.example.anhtuan.quanlyphongtro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detail_PhongTro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail__phong_tro);
+    }
+}
